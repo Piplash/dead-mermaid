0c15903920b6c158871b181a443f9e07d441a691
Felipe Acosta <fibacosta@gmail.com>
2024-12-06 17:51:13 -0300

    footer agregado

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import {
+  faInstagram,
+  faTiktok,
+  faWhatsapp,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
-  selector: 'app-footer',
+  selector: "app-footer",
   standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  imports: [FontAwesomeModule],
+  templateUrl: "./footer.component.html",
+  styleUrl: "./footer.component.css",
 })
 export class FooterComponent {
-
+  public faInstagram = faInstagram;
+  public faTiktok = faTiktok;
+  public faWhatsapp = faWhatsapp;
+  public faFacebook = faFacebook;
 }
